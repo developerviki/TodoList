1738ce1bfeb6adcee2a84d04cea857192aa45dfb
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-02-11 10:58:25 +0530

    delet button update succesfully

--- Api Todo/script.js
+++ Api Todo/script.js
@@ -63,6 +63,11 @@ function addTodo(sno = 1, value,id) {
     let title = prompt("enter new title");
     update(e.target.parentElement.parentElement.id,title)
   })
+
+  delete_button.addEventListener('click',(e)=>{
+    console.log(e.target.parentElement.parentElement.id)
+    deleteTodo(e.target.parentElement.parentElement.id)
+  })
 }
 
 function getTodos(){
@@ -81,9 +86,8 @@ function getTodos(){
         
       });
   }
+
   
-
-
 function add(title) {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
@@ -149,10 +153,12 @@ function addButton(){
     }).showToast();
     return;
 
-  }
-  add(input.value);
+  }{
 
-  input.value = "";
+    add(input.value);
+  
+    input.value = "";
+  }
 }
 
 
@@ -185,7 +191,7 @@ function update(id,title) {
           position: "right",
           stopOnFocus: true, 
           style: {
-            background: "red",
+            background: "green",
             color:"white"
           },
           onClick: function(){}
@@ -200,6 +206,56 @@ function update(id,title) {
     .catch((error) => console.error(error));
 }
 
-// update("67a304be33fb315fb4ec6776","this")
+function deleteTodo(id){
+  const requestOptions = {
+    method: "DELETE",
+    redirect: "follow"
+  };
+  
+  fetch(`http://4.240.85.243:3000/todos/${id}`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      if(result.message == "Todo not found"){
+        Toastify({
+          text: result.message,
+          duration: 3000,
+          destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top",
+          position: "right",
+          stopOnFocus: true, 
+          style: {
+            background: "red",
+            color:"white"
+          },
+          onClick: function(){}
+        }).showToast();
+      } else if(result.message == "Todo deleted successfully"){
+        Toastify({
+          text: result.message,
+          duration: 3000,
+          destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top",
+          position: "right",
+          stopOnFocus: true, 
+          style: {
+            background: "green",
+            color:"white"
+          },
+          onClick: function(){}
+        }).showToast();
+
+        let main = document.getElementById("main-container")
+        main.innerHTML = ""
+        getTodos()
+      }
+    })
+    .catch((error) => console.error(error));
+}
+
+// deleteTodo()
 
 getTodos();  
